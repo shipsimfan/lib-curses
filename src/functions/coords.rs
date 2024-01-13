0facd0d20ee6a55d3a265845d75460ad438cc7b2
Lance Hart <shipsimfan@hotmail.com>
2024-01-12 21:34:35 -0500

    Added coordinate functions

---
+++ src/functions/coords.rs
@@ -0,0 +1,31 @@
+use std::ffi::c_int;
+
+use crate::Window;
+
+#[link(name = "curses")]
+extern "C" {
+    /// Gets the beginning x of `win`
+    pub fn getbegx(win: *mut Window) -> c_int;
+
+    /// Gets the beginning y of `win`
+    pub fn getbegy(win: *mut Window) -> c_int;
+
+    /// Gets the current cursor x of `win`
+    pub fn getcurx(win: *mut Window) -> c_int;
+
+    /// Gets the current cursor  y of `win`
+    pub fn getcury(win: *mut Window) -> c_int;
+
+    /// Gets the maximum x of `win`
+    pub fn getmaxx(win: *mut Window) -> c_int;
+
+    /// Gets the maximum y of `win`
+    pub fn getmaxy(win: *mut Window) -> c_int;
+
+    /// Gets the x position relative to the parent window, or -1 if there isn't one
+    pub fn getparx(win: *mut Window) -> c_int;
+
+    /// Gets the y position relative to the parent window, or -1 if there isn't one
+    pub fn getpary(win: *mut Window) -> c_int;
+
+}
--- src/functions/mod.rs
+++ src/functions/mod.rs
@@ -1,10 +1,12 @@
 mod addstr;
+mod coords;
 mod getch;
 mod initscr;
 mod input_options;
 mod refresh;
 
 pub use addstr::*;
+pub use coords::*;
 pub use getch::*;
 pub use initscr::*;
 pub use input_options::*;
