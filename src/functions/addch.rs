a30b2aa745dc7b3129320eb3fcc0544a7638b86a
Lance Hart <shipsimfan@hotmail.com>
2024-01-13 20:00:51 -0500

    Added echochar functions

--- src/functions/addch.rs
+++ src/functions/addch.rs
@@ -1,6 +1,10 @@
 use crate::{CHType, Window};
 use std::ffi::c_int;
 
+// rustdoc imports
+#[allow(unused_imports)]
+use crate::{refresh, wrefresh};
+
 #[link(name = "curses")]
 extern "C" {
     /// Add a character (with attributes) to a `curses` window, then advance the cursor
@@ -14,5 +18,13 @@ extern "C" {
 
     /// Add a character (with attributes) to a `curses` window, then advance the cursor
     pub fn mvwaddch(window: *mut Window, y: c_int, x: c_int, ch: CHType) -> c_int;
+
+    /// The [`echochar`] routine is equivalent to a call to [`addch`] followed by a call to
+    /// [`refresh`]
+    pub fn echochar(ch: CHType) -> c_int;
+
+    /// The [`wechochar`] routine is equivalent to a call to [`waddch`] followed by a call to
+    /// [`wrefresh`]
+    pub fn wechochar(window: *mut Window, ch: CHType) -> c_int;
 
 }
